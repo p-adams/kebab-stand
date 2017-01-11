c8d372fde1ec0250abc939d63aa659cca23f9a79
Paul Adams <Paul Adams>
2017-01-11 02:51:01 -0500

    added ElementUI

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import locale from 'element-ui/lib/locale/lang/en'
 import store from './store.js'
+
+Vue.use(ElementUI, {locale})
 
 new Vue({
   el: '#app',
